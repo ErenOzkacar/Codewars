c8885797bd1ddcb035234f3f156144131a8525be
ErenOzkacar <ErenOzkacar@icloud.com>
2020-12-15 07:13:10 +0100

    Initial Commit

---
+++ Codewars/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Codewars
+//
+//  Created by Eren Özkacar on 15.12.20.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
