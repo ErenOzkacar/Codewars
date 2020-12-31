b332be573ffb428d33e1934df8f9d6e22f02f4cd
ErenOzkacar <ErenOzkacar@icloud.com>
2020-12-31 12:06:42 +0100

    Double Cola

--- Double Cola/main.cpp
+++ Double Cola/main.cpp
@@ -4,7 +4,7 @@
 //
 //  Created by Eren Özkacar on 24.12.20.
 //
-// Challenge: Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on. Write a program that will return the name of the person who will drink the n-th cola.
+//  Challenge: Sheldon, Leonard, Penny, Rajesh and Howard are in the queue for a "Double Cola" drink vending machine; there are no other people in the queue. The first one in the queue (Sheldon) buys a can, drinks it and doubles! The resulting two Sheldons go to the end of the queue. Then the next in the queue (Leonard) buys a can, drinks it and gets to the end of the queue as two Leonards, and so on. Write a program that will return the name of the person who will drink the n-th cola.
 
 #include <iostream>
 #include <string>
